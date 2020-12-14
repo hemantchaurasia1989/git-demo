bc0aa70686b8243ade7631e7a326308fa7e305d2
hemant chaurasia <hemantchautrasia1989gmail.com>
2020-12-14 11:55:18 +0530

    add if condition to check tha data

--- src/main/java/com/java/versionControlApp/App.java
+++ src/main/java/com/java/versionControlApp/App.java
@@ -1,6 +1,11 @@
 package com.java.versionControlApp;
 
 public class App {
+	
+	public void addData(String data) {
+		if(data.equalsIgnoreCase("Hemant Chaurasia"))
+		System.out.println("Wlcome");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
